47debafc43f59e92659d70ee2e7c39b49ebefe83
Pasan115 <pasanamarasinghe2003@gmail.com>
2024-09-03 11:07:51 +0530

    app js changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import AddQuantity from './AddQuantity';
+import Inventory from './Inventory';
+import OrderRequest from './OrderRequest';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter>
+  <Routes>
+    <Route path = '/' element={<Inventory/>} />
+    <Route path = '/AddQuantity' element={<AddQuantity/>}/>
+    <Route path = '/dashboard' element={<Dashboard/>} />
+    <Route path = '/dashboard/request' element={<OrderRequest/>}/>
+
+    </Routes>
+  </BrowserRouter>
   );
 }
 
 export default App;
+
+/*import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    this.getPosts(); // Added missing semicolon
+  }
+
+  getPosts() {
+    axios.get("http://localhost:8001/posts").then(res => {
+      if (res.data.success) {
+        this.setState({
+          posts: res.data.existingPosts
+        });
+
+        console.log(this.state.posts);
+      }
+    }); // Added missing semicolon
+  }
+
+  render() {
+    return (
+      <div>App</div>
+    );
+  }
+}*/
+
+
