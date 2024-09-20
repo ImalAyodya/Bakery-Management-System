1bcce0e44b5c945fd77999f9b6ef09e9ca553ef3
YeshaniB <malmibandara@gmail.com>
2024-09-20 11:09:52 +0530

    Update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CommercialOrder from './CommercialOrder';
+import Onlineorder from './OnlineOrder';
+import Checkout from './Checkout';
+import Home from './Home';
+import OrderDashboard from './OrderDashboard';
+import OnlineOrderDB from './OnlineOrderDB';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return(
 
+    <BrowserRouter>
+      <Routes>
+        <Route path= "/" element={<CommercialOrder/>}/>
+        <Route path="/Online" element={<Onlineorder/>}/>
+        <Route path="/CheckoutOrder" element={<Checkout/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/orderDashbrd" element={<OrderDashboard/>}/>
+        <Route path="/onlineOrderDashbrd" element={<OnlineOrderDB/>}/>
+        
+        
+
+
+
+        
+      </Routes>
+      
+  </BrowserRouter>
+);}
 export default App;
