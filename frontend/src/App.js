99ef0d089c7dc4cde35500adcdcc76a08e4a7728
YeshaniB <malmibandara@gmail.com>
2024-09-20 11:29:24 +0530

    Conflict resolving

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,13 @@
+import React, { useState } from 'react'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+
+import ProductManagement from './pages/ProductManagement';
+import ProductWastageManagement from './pages/ProductWastageManagement';
+import RequestStaff from './pages/RequestStaff';
+import ProductionForm from './components/ProductionForm';
+import IngredientRequest from './pages/IngredientRequest';
+import UpdateProduct from './pages/updateProduct';
+
 import CommercialOrder from './CommercialOrder';
 import Onlineorder from './OnlineOrder';
 import Checkout from './Checkout';
@@ -5,21 +15,26 @@ import Home from './Home';
 import OrderDashboard from './OrderDashboard';
 import OnlineOrderDB from './OnlineOrderDB';
 
-
-
 function App() {
-  return(
-
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path= "/" element={<CommercialOrder/>}/>
-        <Route path="/Online" element={<Onlineorder/>}/>
-        <Route path="/CheckoutOrder" element={<Checkout/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/orderDashbrd" element={<OrderDashboard/>}/>
-        <Route path="/onlineOrderDashbrd" element={<OnlineOrderDB/>}/>
+        <Route index element={<ProductWastageManagement />} />
+        <Route path="/product-management" element={<ProductManagement />} />
+        <Route path="/request-staff" element={<RequestStaff />} />
+        <Route path='/request-ingredient' element={<IngredientRequest />} />
+        <Route path='/daily-production' element={<ProductionForm />} />
+        <Route path='/updateProduct/:id' element={<UpdateProduct />} />
+
+        <Route path="/" element={<CommercialOrder />} />
+        <Route path="/Online" element={<Onlineorder />} />
+        <Route path="/CheckoutOrder" element={<Checkout />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/orderDashbrd" element={<OrderDashboard />} />
+        <Route path="/onlineOrderDashbrd" element={<OnlineOrderDB />} />
       </Routes>
-      
-  </BrowserRouter>
-);}
+    </BrowserRouter>
+  );
+}
+
 export default App;
