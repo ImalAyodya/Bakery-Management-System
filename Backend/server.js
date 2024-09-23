7fb57c3cbdee0354d30b4a2970d156bca02397af
KavishaGimhani <kavigimhani09@gmail.com>
2024-09-23 08:42:28 +0530

    server js update

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser= require('body-parser');
+const cors = require('cors');
+
+const postRoute = require('./routes/routes')
+app.use(cors()); 
+
+app.use(bodyParser.json());
+app.use(postRoute)
+
+const PORT = 8001;
+//const DB_URL = 'mongodb+srv://kavisha:kavisha2002@employee.tgy9x.mongodb.net/?retryWrites=true&w=majority&appName=Employee'; 
+const DB_URL ='mongodb+srv://malmi:malmi123@cluster0.3dgof.mongodb.net/EmployeeDB?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(DB_URL)
+.then(()=>{
+    console.log('Database Connected');
+})
+
+.catch((error)=> console.log('Database not connected',error));
+
+const server = app.listen(PORT, ()=>{
+    console.log(`node server is listening to ${PORT}`);
+})
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,4 @@
-<<<<<<< Updated upstream
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 import React from 'react';
 import ProductManagement from './pages/ProductManagement';
 import ProductWastageManagement from './pages/ProductWastageManagement';
@@ -73,7 +50,6 @@ function App() {
             </Routes>
         </BrowserRouter>
     );
->>>>>>> Stashed changes
 }
 
 export default App;
