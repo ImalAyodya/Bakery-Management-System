8680787ac945470e6a72051120d5ea7d28bc1e73
AkashMalinga <kshmalinga@gmail.com>
2024-10-04 13:41:49 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import AdminDashboard from './AdminDashboard';
+import Supplier from './Supplier';
+import SupplierForm from './SupplierForm';
+import View from './View';
+import OrderView from './OrderView';
+import OrderForm from './OrderForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='' element={<AdminDashboard />} />
+        <Route path='AdminDashboard' element={<AdminDashboard />} />
+        <Route path="/Supplier" element={<Supplier/>} />
+        <Route path="/SupplierForm" element={<SupplierForm />} />
+        <Route path="/View" element={<View />} />
+        <Route path='/OrderView' element={<OrderView />} />
+        <Route path="/OrderForm" element={<OrderForm />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
