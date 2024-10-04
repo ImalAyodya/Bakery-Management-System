07d7a8c24833e1cbca01e96b9a14f68f55b367de
ImalAyodya <150762145+ImalAyodya@users.noreply.github.com>
2024-10-04 08:56:23 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,12 +7,11 @@ require('dotenv').config();
 
 const app = express();
 
-const port = 5080;
+const port = 8000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// const URL = "mongodb+srv://Ayodya:Ayodya@cluster0.csn4fag.mongodb.net/ProductDB?retryWrites=true&w=majority&appName=Cluster0";
 const URL = "mongodb+srv://malmi:malmi123@cluster0.3dgof.mongodb.net/ProductDB?retryWrites=true&w=majority&appName=Cluster03';";
 
 mongoose.connect(URL, {}).then(() => {
@@ -23,7 +22,7 @@ mongoose.connect(URL, {}).then(() => {
 
 const productRoutes = require('./routes/products.js');
 
-http://localhost:5080/products.
+http://localhost:8000/products.
 
 app.use('/products', productRoutes);
 
